0d7495bc65a48128c0089acb7a762d1de3a22ee0
kyechou <kuanyenchou@gmail.com>
2018-11-01 23:58:35 -0500

    Rename header guard

--- src/error.h
+++ src/error.h
@@ -1,5 +1,5 @@
-#ifndef LUNAR_CALENDAR_ERROR_H
-#define LUNAR_CALENDAR_ERROR_H
+#ifndef _ERROR_H
+#define _ERROR_H
 
 extern void lc_error(const char *format, ...);
 extern const char *lc_strerror(int errnum);

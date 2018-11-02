466c0e12bfac6f14832abd7ec866759fcfe4a8fe
kyechou <kuanyenchou@gmail.com>
2018-11-01 22:26:59 -0500

    Add md5sum

---
+++ src/md5sum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <errno.h>
+#include "error.h"
+#include "md5.h"
+#include "md5sum.h"
+
+int md5sum(unsigned char *digest, const char *filename)
+{
+	FILE *file;
+	MD5_CTX ctx;
+	size_t size;
+	unsigned char data[1024];
+
+	if ((file = fopen(filename, "rb")) == NULL) {
+		lc_error("failed to open %s: %s\n", filename, lc_strerror(errno));
+		return -1;
+	}
+
+	MD5_Init(&ctx);
+	while ((size = fread(data, 1, 1024, file)) != 0)
+		MD5_Update(&ctx, data, size);
+	MD5_Final(digest, &ctx);
+
+	if (fclose(file) == EOF) {
+		lc_error("failed to close %s: %s\n", filename, lc_strerror(errno));
+		return -1;
+	}
+	return 0;
+}
---
+++ src/md5sum.h
@@ -0,0 +1,6 @@
+#ifndef _MD5SUM_H
+#define _MD5SUM_H
+
+extern int md5sum(unsigned char *digest, const char *filename);
+
+#endif
